1d9a2fcc60231d9fed1af6c2b9715fbb490807d6
Takumi Hara <takumihara1226@gmail.com>
2022-06-02 17:20:52 +0900

    feat: add build.rs

---
+++ build.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+
+const ASM_DIR_PATH: &str = "asm";
+const ASM_FILE: &str = "asm/context.S";
+const O_FILE: &str = "asm/context.o";
+const LIB_FILE: &str = "asm/libcontext.a";
+
+fn main() {
+    Command::new("cc")
+        .args(&[ASM_FILE, "-c", "-fPIC", "-o"])
+        .arg(O_FILE)
+        .status()
+        .unwrap();
+    Command::new("ar")
+        .args(&["crus", LIB_FILE, O_FILE])
+        .status()
+        .unwrap();
+
+    // asmをライブラリ検索パスに追加
+    println!("cargo:rustc-link-search=native={}", ASM_DIR_PATH);
+    // libcontext.aという静的ライブラリをリンク
+    println!("cargo:rustc-link-lib=static=context");
+    // asm/context.Sというファイルに依存
+    println!("cargo:return-if-changed=asm/context.S");
+}
