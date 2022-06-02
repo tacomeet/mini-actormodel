7963c43cc255c07d5907ade1ae026604aa2c4bb2
Takumi Hara <takumihara1226@gmail.com>
2022-06-02 20:06:54 +0900

    feat: add main func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod green;
+
+use std::io::Write;
+
+fn mash() {
+    green::spawn(ortega, 2 * 1024 * 1024);
+    for _ in 0..10 {
+        std::io::stdout().write(b"Mash!\n").unwrap();
+        green::schedule();
+    }
+}
+
+fn ortega() {
+    for _ in 0..10 {
+        std::io::stdout().write(b"Gaia! \n").unwrap();
+        green::schedule();
+    }
+}
+
+fn gaia() {
+    green::spawn(mash, 2 * 1024 * 1024);
+    for _ in 0..10 {
+        std::io::stdout().write(b"Gaia! \n").unwrap();
+        green::schedule();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    green::spawn_from_main(gaia, 2 * 1024 * 1024);
 }
